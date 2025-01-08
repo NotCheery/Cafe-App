01e9faa84da69d734e74bea02faad49d51096330
Tahia <140080069+NotCheery@users.noreply.github.com>
2025-01-08 11:01:17 -0500

    created react app

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './index.css';
+
+const App = () => {
+
+  return (
+    <div className="App">
+      <h1>Cafe</h1>
+      <h2>Welcome! Click on Instructions to get to know how to play, or click on Start to play!</h2>
+      <button>Instructions</button>
+      <button>Start</button>
+    </div>
+  )
+}
+
+export default App
