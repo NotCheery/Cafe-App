ee370cf4ba9cb50e29964eb9c5c88b5e25f793a4
Tahia <140080069+NotCheery@users.noreply.github.com>
2025-01-14 13:03:43 -0500

    Updated ui space theme

--- src/components/make_order.jsx
+++ src/components/make_order.jsx
@@ -3,13 +3,18 @@ const MakeOrder = ({onBack}) => {
     return (
         <div className="make-order">
             <h2>Make Order</h2>
+
             <div className="order-buttons">
-            <button>Sugar</button>
-            <button>Milk</button>
-            <button>Hot Water</button>
-            <button>Coffee Powder</button>
-            <button onClick={onBack}>Back</button>
+                <button>Sugar</button>
+                <button>Milk</button>
+                <button>Hot Water</button>
+                <button>Coffee Powder</button>
+                <button>Sprinkles</button>
+                <button>Ice</button>
+                <button>candycane</button>
+                <button>Frosting</button>
             </div>
+            <button onClick={onBack}>Back</button>
         </div>
 
     );
